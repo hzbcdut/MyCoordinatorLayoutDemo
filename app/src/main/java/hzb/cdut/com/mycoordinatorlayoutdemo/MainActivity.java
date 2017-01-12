0be2e22214ca6417b79bc3223e92ea61b8109a00
huzhengbiao <hans.hu@livestar.com>
2017-01-12 17:36:58 +0800

    Snackbar setAction(“”， listener)  listener为null时，Snackbar会隐藏，FloatingActionButton会下移
     listener不为null时，否则需要手动获取snackbar.getView(）设置View可见状态为GONG
    
     右滑可以隐藏snackbar, 点击右侧可点区域也会隐藏snackbar，但FloatingActionButton不会下移。

--- app/src/main/java/hzb/cdut/com/mycoordinatorlayoutdemo/MainActivity.java
+++ app/src/main/java/hzb/cdut/com/mycoordinatorlayoutdemo/MainActivity.java
@@ -1,9 +1,9 @@
 package hzb.cdut.com.mycoordinatorlayoutdemo;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,15 +16,27 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "点击进入第二个Activity", Snackbar.LENGTH_LONG)
-                        .setAction("sure", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                                MainActivity.this.startActivity(intent);
-                            }
-                        }).show();
+                // 弹出Snackbar右滑可以隐藏
+                final Snackbar snackbar = Snackbar.make(v, "点击进入第二个Activity", Snackbar.LENGTH_LONG);
+                snackbar.setAction("sure", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                        MainActivity.this.startActivity(intent);
+                        // 这样设置FloatingActionButton才会在Snackbar弹出后下移
+                        snackbar.getView().setVisibility(View.GONE);
+                        //                        snackbar.dismiss(); // 没有效果
+                    }
+                }).show();
             }
         });
+
+//        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Snackbar.make(v, "Snackbar可以左滑取消哦~", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
