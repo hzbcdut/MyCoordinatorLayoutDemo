15b27097e8889d518d2bd6d77a3d22c70365422d
huzhengbiao <hans.hu@livestar.com>
2017-01-12 17:01:32 +0800

    Android滑动到顶部悬停

--- app/src/main/java/hzb/cdut/com/mycoordinatorlayoutdemo/MainActivity.java
+++ app/src/main/java/hzb/cdut/com/mycoordinatorlayoutdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package hzb.cdut.com.mycoordinatorlayoutdemo;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "点击进入第二个Activity", Snackbar.LENGTH_LONG)
+                        .setAction("sure", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                                MainActivity.this.startActivity(intent);
+                            }
+                        }).show();
+            }
+        });
     }
 }
